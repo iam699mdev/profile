6edc11a10cce3c2e669161a27c22edf8f8ca3cc2
Markx <69718125+TakshyaRaj@users.noreply.github.com>
2022-10-14 23:24:26 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -44,34 +44,34 @@ function scrollActive(){
 /*===== SCROLL REVEAL ANIMATION =====*/
 const sr = ScrollReveal({
     origin: 'top',
-    distance: '80px',
-    duration: 2000,
+    distance: '30px',
+    duration: 200,
     reset: true
 })
 
 /*SCROLL HOME*/
 sr.reveal('.home__title', {})
-sr.reveal('.home__scroll', {delay: 200})
+sr.reveal('.home__scroll', {delay: 20})
 sr.reveal('.home__img', {origin:'right', delay: 400})
 
 /*SCROLL ABOUT*/
-sr.reveal('.about__img', {delay: 500})
-sr.reveal('.about__subtitle', {delay: 300})
-sr.reveal('.about__profession', {delay: 400})
-sr.reveal('.about__text', {delay: 500})
-sr.reveal('.about__social-icon', {delay: 600, interval: 200})
+sr.reveal('.about__img', {delay: 50})
+sr.reveal('.about__subtitle', {delay: 30})
+sr.reveal('.about__profession', {delay: 40})
+sr.reveal('.about__text', {delay: 50})
+sr.reveal('.about__social-icon', {delay: 60, interval: 200})
 
 /*SCROLL SKILLS*/
 sr.reveal('.skills__subtitle', {})
 sr.reveal('.skills__name', {distance: '20px', delay: 50, interval: 100})
-sr.reveal('.skills__img', {delay: 400})
+sr.reveal('.skills__img', {delay: 40})
 
 /*SCROLL PORTFOLIO*/
 sr.reveal('.portfolio__img', {interval: 200})
 
 /*SCROLL CONTACT*/
 sr.reveal('.contact__subtitle', {})
-sr.reveal('.contact__text', {interval: 200})
+sr.reveal('.contact__text', {interval: 20})
 sr.reveal('.contact__input', {delay: 400})
 sr.reveal('.contact__button', {delay: 600})
 
